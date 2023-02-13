4d0dfb6f6d57bffa94751156a777d9630df9e3d1
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-13 20:52:33 +0530

    Single player at start position 0

---
+++ Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class Game
+    {
+        
+        public static void StartGame()
+        {
+            int position = 0;
+            int player1;
+        Console.WriteLine("Single player at start position is : " + position);
+        }
+    }
+}
