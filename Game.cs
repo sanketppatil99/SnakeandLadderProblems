16c690a026dc586db3943ad4a47b359abf844596
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-13 21:24:41 +0530

    Plyer check the options

--- Game.cs
+++ Game.cs
@@ -7,19 +7,35 @@ using System.Threading.Tasks;
 namespace SnakeAndLadder
 {
     public class Game
-    {       
-        public static void StartGame()
+    {               
+        public static void CheckOptions()
         {
             int position = 0;
-            int player1;
-            Console.WriteLine("Single player at start position is : " + position);
-        }
-
-        public static void PlayerRollDice()
-        {
             Random random = new Random();
             int diceValue = random.Next(1,7);
             Console.WriteLine("Plyer Roll Dice gate number is : "+diceValue);
+
+            Random random2 = new Random();
+            int option = random2.Next(0,3);
+            if(option == 0)
+            {
+                Console.WriteLine("Snake");
+                Console.WriteLine("Player moves behind by the number of position recieved in the dice : "+diceValue);
+                position = position - diceValue;
+                Console.WriteLine(position);
+            }
+            else if (option == 1)
+            {
+                Console.WriteLine("Ladder");
+                Console.WriteLine("Player moves ahead by the number of position recieved in the dice : " + diceValue);
+                position = position + diceValue;
+                Console.WriteLine(position);
+            }
+            else
+            {
+                Console.WriteLine("No play");
+                Console.WriteLine("Player will stay in the same position");
+            }
         }
     }
 }
