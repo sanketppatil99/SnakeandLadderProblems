91afe92dd51ea1128a12787286f9ae7e2d0de2b4
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-13 21:04:57 +0530

    Plyer roll the dice gate number btw 1 to 6

--- Game.cs
+++ Game.cs
@@ -7,13 +7,19 @@ using System.Threading.Tasks;
 namespace SnakeAndLadder
 {
     public class Game
-    {
-        
+    {       
         public static void StartGame()
         {
             int position = 0;
             int player1;
-        Console.WriteLine("Single player at start position is : " + position);
+            Console.WriteLine("Single player at start position is : " + position);
+        }
+
+        public static void PlayerRollDice()
+        {
+            Random random = new Random();
+            int diceValue = random.Next(1,7);
+            Console.WriteLine("Plyer Roll Dice gate number is : "+diceValue);
         }
     }
 }
